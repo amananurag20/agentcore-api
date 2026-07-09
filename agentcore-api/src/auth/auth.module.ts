51834aa16e89c181c0740917ce4d024220efc023
Aman Anurag <131232447+amananurag20@users.noreply.github.com>
2026-07-09 16:07:11 +0530

    changes added

--- agentcore-api/src/auth/auth.module.ts
+++ agentcore-api/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuditModule } from '../audit/audit.module';
 import { OrganizationsModule } from '../organizations/organizations.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -21,6 +22,7 @@ import { JwtStrategy } from './jwt.strategy';
     }),
     AuditModule,
     OrganizationsModule,
+    PrismaModule,
     UsersModule,
   ],
   controllers: [AuthController],
